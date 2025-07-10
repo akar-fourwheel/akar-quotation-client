ca5a5d90bd795d5ba0928b465f391719106099bc
lakshya <panchallakshya64@gmail.com>
2025-07-10 14:50:55 +0530

    merge

--- src/context/auth/ProtectedRoute.jsx
+++ src/context/auth/ProtectedRoute.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { AuthContext } from "./AuthProvider";
 import { Outlet, Navigate, useLocation } from "react-router";
+import Loader from "../../Components/Loader/Loader";
 
 const ProtectedRoute = ({ roles, children }) => {
   const { isAuthenticated, isLoading, role } = useContext(AuthContext);
@@ -8,7 +9,7 @@ const ProtectedRoute = ({ roles, children }) => {
   const location = useLocation();
 
   if (isLoading) {
-    return <div>Loading...</div>; // You might want to replace this with a proper loading component
+    return <Loader />; // You might want to replace this with a proper loading component
   }
 
   if (!isAuthenticated) {
